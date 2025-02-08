ed9a309b7ce5291c99cf2529c2b9b228fecb4cba
ChronoTrigger64 <sydneey@hawaii.edu>
2025-02-08 00:02:22 -1000

    added all the files, and also added user input

---
+++ app.js
@@ -0,0 +1,15 @@
+    var buttontest = document.getElementById('test');
+    var buttoning = document.getElementById('btest');
+
+    buttoning.addEventListener("click", buttontesting);
+
+    function buttontesting() {
+        buttontest.innerHTML = "don't touch that!";
+        console.log('hhhhh');
+    }
+
+    document.getElementById('displayButton').addEventListener('click', function() {
+        var userInput = document.getElementById('userInput').value;
+        var displayText = document.getElementById('displayText');
+        displayText.innerHTML = 'You inputted ' + userInput;
+    });
