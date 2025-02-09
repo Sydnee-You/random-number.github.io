5263bb33515a038e3eb609c6f4a78ee8d9f05c7b
ChronoTrigger64 <sydneey@hawaii.edu>
2025-02-08 19:48:46 -1000

    added comments, adjusted code formatting in js.

--- app.js
+++ app.js
@@ -41,16 +41,20 @@ var howManyCheck = document.getElementById("howManyCheck");
 //put all the above variables into an array of objects.
 //default display is for setting the display back to default when the user deletes or inputs another value
 const elements = [
-    {button: minButton, input: minRange, display: minCheck, defaultDisplay: "Min Number: &nbsp;"},
-    {button: maxButton, input: maxRange, display: maxCheck, defaultDisplay: "Max Number: &nbsp;"},
-    {button: howManyButton, input: howMany, display: howManyCheck, defaultDisplay: "How Many: &nbsp;"}
+    { button: minButton, input: minRange, display: minCheck, defaultDisplay: "Min Number: &nbsp;" },
+    { button: maxButton, input: maxRange, display: maxCheck, defaultDisplay: "Max Number: &nbsp;" },
+    { button: howManyButton, input: howMany, display: howManyCheck, defaultDisplay: "How Many Numbers: &nbsp;" }
 ];
 
 /*loop through each object in elements array to make it such that when you add an input and click on their
 respective button, the paragraph below clarifies what you inputted.
 */
 elements.forEach(function (element) {
-    element.button.addEventListener("click", function() {
+    element.button.addEventListener("click", function () {
+        /*reset the display of what the user inputted back to default every time the user changes the value
+          otherwise, every user input in the input (minRange,maxRange, howMany) would add onto each previous user
+          input, which we don't want.
+        */
         element.display.innerHTML = element.defaultDisplay;
         element.display.innerHTML += element.input.value;
     });
