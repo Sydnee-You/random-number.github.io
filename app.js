1a9a3494c30bd958fab1d0a55a81d0f841aeb3f7
ChronoTrigger64 <sydneey@hawaii.edu>
2025-02-10 01:23:03 -1000

    added checking to make sure min value isn't greater than max value and vise versa
    also added checking for the how many option being less than or equal to the overall range of numbers.

--- app.js
+++ app.js
@@ -17,33 +17,41 @@ document.getElementById('displayButton').addEventListener('click', function () {
 /*get the minimum value from the user (minRange), submit button for min value input (minButton),
 and paragraph that reminds user of their input and store them into variables (minCheck).
 */
-var minRange = document.getElementById("minRange");
 var minButton = document.getElementById("minButton");
+var minRange = document.getElementById("minRange");
+//holds user input
+var minHold = 0;
 var minCheck = document.getElementById("minCheck");
-
+var minHasInput = false;
 
 /*get the maximum value from the user (maxRange), submit button for max value input (maxButton),
 and paragraph that reminds user of their input and store them into variables (maxCheck).
 */
-var maxRange = document.getElementById("maxRange");
 var maxButton = document.getElementById("maxButton");
+var maxRange = document.getElementById("maxRange");
+//holds user input
+var maxHold = 0;
 var maxCheck = document.getElementById("maxCheck");
+var maxHasInput = false;
 
 
 /*get the amount of numbers to randomly choose from the user (howMany), 
 submit button for amount of numbers input (howManyButton),
 and paragraph that reminds user of their input and store them into variables (howManyCheck).
 */
-var howMany = document.getElementById("howMany");
 var howManyButton = document.getElementById("howManyButton");
+var howMany = document.getElementById("howMany");
+//holds user input
+var howManyHold = 0;
 var howManyCheck = document.getElementById("howManyCheck");
+var howManyHasInput = false;
 
 //put all the above variables into an array of objects.
 //default display is for setting the display back to default when the user deletes or inputs another value
 const elements = [
-    { button: minButton, input: minRange, display: minCheck, defaultDisplay: "Min Number: &nbsp;" },
-    { button: maxButton, input: maxRange, display: maxCheck, defaultDisplay: "Max Number: &nbsp;" },
-    { button: howManyButton, input: howMany, display: howManyCheck, defaultDisplay: "How Many Numbers: &nbsp;" }
+    { button: minButton, input: minRange, hold: minHold, display: minCheck, defaultDisplay: "Min Number: &nbsp;", hasInput: minHasInput },
+    { button: maxButton, input: maxRange, hold: maxHold, display: maxCheck, defaultDisplay: "Max Number: &nbsp;", hasInput: maxHasInput },
+    { button: howManyButton, input: howMany, hold: howManyHold, display: howManyCheck, defaultDisplay: "How Many Numbers: &nbsp;", hasInput: howManyHasInput }
 ];
 
 /*loop through each object in elements array to make it such that when you add an input and click on their
@@ -56,7 +64,102 @@ elements.forEach(function (element) {
           input, which we don't want.
         */
         element.display.innerHTML = element.defaultDisplay;
-        element.display.innerHTML += element.input.value;
+        console.log(element);
+
+        // Convert the input value to a number
+        element.hold = Number(element.input.value);
+        console.log(element.hold);
+
+        //make sure user only inputs integers
+        if (Number.isInteger(element.hold)) {
+            console.log("it is int");
+
+            //if user enters the min number
+            if (element.button == minButton) {
+                console.log("hi");
+                //if user has already entered the max number, check to make sure min number is not bigger than max number
+                if (elements[1].hasInput == true) {
+                    
+                    if(element.hold > elements[1].hold) {
+                        element.hold = 0;
+                        element.hasInput = false;
+                        element.display.innerHTML += " min number needs to be smaller than max number";
+                        
+                    } else {
+                        element.hasInput = true;
+                        element.display.innerHTML += element.input.value;
+                    }
+                } else {
+                    element.hasInput = true;
+                    element.display.innerHTML += element.input.value;
+
+                }
+
+                
+            }
+
+            //if user enters max number
+            else if(element.button == maxButton) {
+                //if user has already entered the min number, check to make sure it is not bigger than max number
+                if(elements[0].hasInput == true) {
+
+                    if(elements[0].hold > element.hold) {
+                        element.hold = 0;
+                        element.hasInput = false;
+                        element.display.innerHTML += " max number needs to be bigger than min number";
+                    } else {
+                        element.hasInput = true;
+                        element.display.innerHTML += element.input.value;
+                    }
+                } else {
+                    element.hasInput = true;
+                    element.display.innerHTML += element.input.value;
+                }
+
+            } 
+            else if(element.button == howManyButton) {
+                console.log("how many button");
+                //this is if user inputs for the how many option
+                if((elements[0].hasInput == true) && (elements[1].hasInput == true)) {
+                    var range = (elements[1].hold - elements[0].hold) + 1;
+                    console.log("range is " + range);
+
+                    if(element.hold > range) {
+                        element.hold = 0;
+                        element.hasInput = false;
+                        element.display.innerHTML += " How many numbers should be smaller than the range";
+                    } else {
+                        element.hasInput = true;
+                        element.display.innerHTML += element.input.value;
+                    }
+                } else {
+                    element.hasInput = true;
+                    element.display.innerHTML += element.input.value;
+                }
+            }
+            //if all values are successfully inputted, get random values of the given range of numbers
+            if(elements[0].hasInput && elements[1].hasInput && elements[2].hasInput) {
+                console.log("they are all here");
+                //the full range of numbers from the min number to max number, regardless of what the user put
+                //for the "how many" value
+                const theFullRange = [];
+
+                for(var i = elements[0].hold; i <= elements[1].hold; i++) {
+                    theFullRange.push(i);
+                    console.log("added " + i + " to the array list");
+                }
+
+            } else {
+                console.log("they are not all here");
+            }
+
+        } else {
+            element.hold = 0;
+            element.hasInput = false;
+            element.display.innerHTML += " Integers only please";
+        }
     });
 
 });
+
+
