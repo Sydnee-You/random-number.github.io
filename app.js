472cc2a0a86647325e39119537ba0ceb667f63bf
ChronoTrigger64 <sydneey@hawaii.edu>
2025-02-10 11:41:03 -1000

    finished inputting navbar

--- app.js
+++ app.js
@@ -1,19 +1,3 @@
-var buttontest = document.getElementById('test');
-var buttoning = document.getElementById('btest');
-
-buttoning.addEventListener("click", buttontesting);
-
-function buttontesting() {
-    buttontest.innerHTML = "don't touch that!";
-    console.log('hhhhh');
-}
-
-document.getElementById('displayButton').addEventListener('click', function () {
-    var userInput = document.getElementById('userInput').value;
-    var displayText = document.getElementById('displayText');
-    displayText.innerHTML = 'You inputted ' + userInput;
-});
-
 /*get the minimum value from the user (minRange), submit button for min value input (minButton),
 and paragraph that reminds user of their input and store them into variables (minCheck).
 */
